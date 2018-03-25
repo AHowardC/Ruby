4d9c6b40a37186db2f6193e470074559477251cf
AHowardC <achsvgusaeng@yahoo.com>
2018-03-25 17:34:25 -0400

    fc

---
+++ methods.rb
@@ -0,0 +1,10 @@
+# .to_s converts into a string
+# .to_i converts into an interger
+# .to_a converts into an array
+# .reverse reverses a string
+# .length returns the length
+# .max returns largest number in an array
+# .sort
+str = 'string'
+arr = [1,2,6,5,3,7,5,3,5]
+bool = true || false
